1fe7581e29de155c4a391867d83b180339ac62bd
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-08-16 10:21:55 +0200

    Added some style to the activitylist and homepage

---
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,37 @@
+import { observer } from "mobx-react-lite";
+import { Header, Menu } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import Calendar from "react-calendar";
+
+export default observer(function ActivityFilters() {
+  // const {
+  //   activityStore: { predicate, setPredicate },
+  // } = useStore();
+
+  return (
+    <>
+      <Menu vertical size="large" style={{ width: "100%", marginTop: 26 }}>
+        <Header icon="filter" attached color="teal" content="Filters" />
+        <Menu.Item
+          content="All Activites"
+          // active={predicate.has("all")}
+          // onClick={() => setPredicate("all", "true")}
+        />
+        <Menu.Item
+          content="I'm going"
+          // active={predicate.has("isGoing")}
+          // onClick={() => setPredicate("isGoing", "true")}
+        />
+        <Menu.Item
+          content="I'm hosting"
+          // active={predicate.has("isHost")}
+          // onClick={() => setPredicate("isHost", "true")}
+        />
+      </Menu>
+      <Header />
+      <Calendar
+        // onChange={(date: any) => setPredicate("startDate", date as Date)}
+      />
+    </>
+  );
+});
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,11 +1,42 @@
-import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { observer } from 'mobx-react-lite';
+import { Link } from 'react-router-dom';
+import { Button, Container, Header, Segment, Image } from "semantic-ui-react";
+import { useStore } from '../../app/stores/store';
+// import LoginForm from '../users/LoginForm';
+// import RegsiterForm from '../users/RegsiterForm';
 
-export default function HomePage() {
-  return (
-    <Container style={{marginTop: '7em'}}>
-      <h1>HomePage</h1>
-      <h3>Go to  <Link to='/activities'> Activities</Link> </h3>
-    </Container>
-  )
-}
+export default observer(function HomePage() {
+    // const { userStore, modalStore } = useStore();
+    return (
+        <Segment inverted textAlign='center' vertical className='masthead' >
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+                    Reactivities
+                </Header>
+                <Header as='h2' inverted content={`Welcome back to Reactivities`} />
+                <Button as={Link} to='/activities' size='huge' inverted>
+                            Take me to the activities!
+                        </Button>
+                {/* {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content={`Welcome back ${userStore.user?.displayName}`} />
+                        <Button as={Link} to='/activities' size='huge' inverted>
+                            Go to activities!
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
+                            Login!
+                        </Button>
+                        <Button onClick={() => modalStore.openModal(<RegsiterForm />)} size='huge' inverted>
+                            Register
+                        </Button>
+                    </>
+
+                )} */}
+            </Container>
+        </Segment>
+    )
+})
